742839d403b864f2d5a17f5b0a44abd468f03f28
Adib <adibahsan@iut-dhaka.edu>
2024-03-11 19:26:12 +0600

    integrated task adding + deleting flow

--- App.tsx
+++ App.tsx
@@ -1,18 +1,56 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, TextInput, Button, FlatList, Text, Alert, TouchableOpacity } from 'react-native';
+import { loadTasks, saveTasks } from './util/storage';
 
 export default function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState<string[]>([]);
 
+  useEffect(() => {
+    const initTasks = async () => {
+      const loadedTasks = await loadTasks();
+      setTasks(loadedTasks);
+    };
+    initTasks();
+  }, []);
+
   const addTask = () => {
-    setTasks(currentTasks => [...currentTasks, task]);
+    if (!task.trim()) return;
+    const newTasks = [...tasks, task];
+    setTasks(newTasks);
     setTask('');
+    saveTasks(newTasks).then(r => console.log('added tasks', newTasks));
   };
+
+  const deleteTask = (index: number) => {
+    const newTasks = tasks.filter((_, taskIndex) => taskIndex !== index);
+    setTasks(newTasks);
+    saveTasks(newTasks).then(r => console.log('deleted tasks', newTasks))
+  };
+
+  const renderItem = ({ item, index }: { item: string; index: number }) => (
+      <View style={styles.taskItem}>
+        <Text>{item}</Text>
+        <TouchableOpacity onPress={() => deleteTask(index)}>
+          <Text style={styles.deleteButton}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+  );
 
   return (
       <View style={styles.container}>
-        <Text>Some Tasks to do here !!!</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setTask}
+            value={task}
+            placeholder="Add a new task"
+        />
+        <Button onPress={addTask} title="Add Task" color="#841584" />
+        <FlatList
+            data={tasks}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+        />
       </View>
   );
 }
@@ -20,8 +58,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5a7a7',
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  taskItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    marginVertical: 5,
+    backgroundColor: '#f9c2ff',
+    borderRadius: 5,
+  },
+  deleteButton: {
+    color: 'red',
   },
 });
